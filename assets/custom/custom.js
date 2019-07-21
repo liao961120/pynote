0786c48fc1cd595100cc43ccc3651c8bc3e3383e
liao <liao961120@gmail.com>
2019-07-21 15:35:43 +0800

    fix JS not loading

--- assets/custom/custom.js
+++ assets/custom/custom.js
@@ -1,20 +1,18 @@
 // Put your custom javascript here
 
-document.addEventListener('DOMContentLoaded', () =>{
-    // get chapter urls from sidebar
-    var li = document.querySelector('.c-sidebar__chapters').querySelectorAll(':nth-child(n+5)');
-    var ch_urls = []; 
-    for (i=0; i<li.length; ++i) {
-        if (li[i].querySelector('a')) 
-            ch_urls.push(li[i].querySelector('a').href);
-    }
+// get chapter urls from sidebar
+var li = document.querySelector('.c-sidebar__chapters').querySelectorAll(':nth-child(n+5)');
+var ch_urls = []; 
+for (i=0; i<li.length; ++i) {
+    if (li[i].querySelector('a')) 
+        ch_urls.push(li[i].querySelector('a').href);
+}
 
-    // modify css property: counter-reset 
-    for (i=0; i<ch_urls.length; ++i) {
-        if (ch_urls[i] == document.URL) {
-            document.querySelector('body').style = `counter-reset: h1 ${i}`
-            break
-        };
-    }
-});
+// modify css property: counter-reset 
+for (i=0; i<ch_urls.length; ++i) {
+    if (ch_urls[i] == document.URL) {
+        document.querySelector('body').style = `counter-reset: h1 ${i}`
+        break
+    };
+}
 
