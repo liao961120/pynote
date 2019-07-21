91235aa6294053c2ad495f983a0df1b07fd6f067
liao <liao961120@gmail.com>
2019-07-21 16:12:37 +0800

    update

--- assets/custom/custom.js
+++ assets/custom/custom.js
@@ -1,18 +1,29 @@
 // Put your custom javascript here
 
-// get chapter urls from sidebar
-var li = document.querySelector('.c-sidebar__chapters').querySelectorAll(':nth-child(n+5)');
-var ch_urls = []; 
-for (i=0; i<li.length; ++i) {
-    if (li[i].querySelector('a')) 
-        ch_urls.push(li[i].querySelector('a').href);
+if ( document.readyState !== 'loading' ) {
+    setInterval(function() {changeSectionNum();}, 100)
+} else {
+    document.addEventListener('DOMContentLoaded', function () {
+        changeSectionNum();
+    });
 }
 
-// modify css property: counter-reset 
-for (i=0; i<ch_urls.length; ++i) {
-    if (ch_urls[i] == document.URL) {
-        document.querySelector('body').style = `counter-reset: h1 ${i}`
-        break
+
+function changeSectionNum() {
+    // get sidebar chapter num info
+    var li = document.querySelector('.c-sidebar__chapters').querySelectorAll(':nth-child(n+5)');
+    var ch_urls = []; 
+    for (i=0; i<li.length; ++i) {
+        if (li[i].querySelector('a')) 
+            ch_urls.push(li[i].querySelector('a').href);
     };
-}
 
+    // modify css property: counter-reset 
+    for (i=0; i<ch_urls.length; ++i) {
+        if (ch_urls[i] == document.URL) {
+            document.querySelector('body').style = `counter-reset: h1 ${i}`
+            break
+        };
+    };
+
+}
