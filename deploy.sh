72d28be3c08412999fb982785d0b9d535103026b
liao <liao961120@gmail.com>
2020-07-23 20:29:31 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env sh
 set -e
 
+read -p "Commit message: " msg
+
 jupyter-book build ./
 echo 'https://pynote.netlify.app/* https://pynote.yongfu.name/:splat 301!' > _redirects
 
-read -p "Commit message: " msg
 git add -A
 git commit -m "${msg}"
 git push origin master
